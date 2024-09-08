3f29666c13864172a8e82d16f8ccd72183faa2d5
marens-b <mariraujo@hotmail.com>
2024-09-08 18:20:36 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function pesquisar(){
+let section = document.getElementById 
+("resultados-pesquisa");
+console.log(section);
+
+let resultados = ""
+
+// para cada dado dentro da lista de dados
+for (let dado of dados) {
+    resultados += `
+<div class="item-resultado">
+        <img src=${dado.imagem} class="Imagens" width="130" height="200">
+        <div class="texto">
+        <h2>
+        <a href=${dado.link} target="_blank">${dado.titulo}</a> 
+        </h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        </div>
+`
+};
+
+section.innerHTML = resultados }
+
---
+++ dados.js
@@ -0,0 +1,15 @@
+let dados = [
+    {   imagem: "Imagens/Aerith.jpg",
+        titulo : "Aerith Gaisnborough",
+        descricao : "Uma vendedora de flores que vive nas favelas do Setor 5, Aerith tem estado sob vigilância da Shinra durante toda a sua vida por causa de sua origem única. Capaz de sentir a força vital do planeta, ela oferece suporte mágico aos seus companheiros.",
+        link : "https://finalfantasy.fandom.com/wiki/Aerith_Gainsborough",
+    },
+    {   imagem: "Imagens/Cloud.jpg", 
+        titulo : "Cloud Strife",
+        descricao : "Ex-SOLDIER de Primeira Classe. Após abandonar a Shinra, Cloud começou a trabalhar como mercenário de aluguel em Midgar. Com seu confiável espadão em mãos, ele sempre garante terminar os seus trabalhos. Cloud reencontra Aerith (vista primeiro no setor 8) após complicações em uma missão no reator 5 onde é separado de Barret e Tifa por uma queda alta, sendo salvo apenas pela cama de flores feita por Aerith na igreja do setor 5.",
+        link : "https://finalfantasy.fandom.com/wiki/Cloud_Strife",
+    },
+    ];
+
+   
+   
